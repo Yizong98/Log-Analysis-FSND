ec63199f13cb82e3c7cfda9d2c6bed2ab1f14a2c
yizong98 <yizong@ucsd.edu>
2019-02-26 09:24:31 -0800

    doc: update readme

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-Desgin
--------------
-* Utilize psycopg2 and print the results of the queries through the connection with database.
+# Logs Analysis - Udacity
+### Full Stack Web Development ND
+_______________________
+## About
+This project is about analyzing backend data through the connection to the database.
+Questions to answer:
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
+
+## Prerequisites
+* Python 2 [https://www.python.org/downloads/]
+* Vagrant [https://www.vagrantup.com/]
+* VirtualBox 3 [https://www.virtualbox.org/wiki/Download_Old_Builds_5_1]
+* Environment [https://s3.amazonaws.com/video.udacity-data.com/topher/2018/April/5acfbfa3_fsnd-virtual-machine/fsnd-virtual-machine.zip]
+* Database [https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip]
 
 Usage Manual
 -------------
-* To run, use terminal: python logdb.py in the residing folder. 
+* To run, go the folder with logdb.py
+* set up vagrant with command: vagrant up
+* then use the command: vagrant ssh
+* use terminal: python logdb.py in the residing folder. 
+* To load the data, cd into the vagrant directory and use the command: psql -d news -f newsdata.sql
 
 
--- logdb.py
+++ logdb.py
@@ -1,32 +1,40 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python2
 import psycopg2
 
 
 def set_db(query):
     """Return all posts from the 'database', most recent first."""
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute(query)
-    val = c.fetchall()
-    db.close()
-    return val
+    try:
+        db = psycopg2.connect("dbname=news")
+        c = db.cursor()
+        c.execute(query)
+        val = c.fetchall()
+        db.close()
+        return val
+    except psycopg2.Error as e:
+        print("Unable to connect to the database")
+        print(e.pgerror)
+        print(e.diag.message_detail)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-    q1 = """ select aubook.name, sum(num) as tot from
-    (select articles.title, num from articles,
+    q1 = """  select aubook.name, sum(num) as tot from
+    (select articles.title as title, num from articles,
     (select articles.slug as slug,
     count(log.path) as num from log,articles
     where log.path  like '%' ||articles.slug || '%'
+    and log.status not like '%404%'
     group by articles.slug order by num desc) as top_three
-    where articles.slug = top_three.slug order by num desc) as rank,
-    (select title, name
+    where articles.slug = top_three.slug order by num desc) as rank
+    join (select title, name
   from articles, authors
-  where articles.author = authors.id)
-  as aubook group by aubook.name order by tot;"""
+  where articles.author = authors.id) as           
+  aubook on rank.title = aubook.title group by aubook.name 
+  order by tot desc;"""
     q2 = """select articles.title, num from articles,
     (select articles.slug as slug, count(log.path) as num from log,articles
-    where log.path  like '%' ||articles.slug || '%'
+    where log.path  like '%' ||articles.slug || '%' and log.status not like '%404%'
     group by articles.slug order by num desc limit 3) as top_three
     where articles.slug = top_three.slug order by num desc;"""
     q3 = """select * from (select to_char(time,'MonthDD,YYYY') as date,ROUND((
@@ -45,3 +53,17 @@ if __name__ == '__main__':
     print "\nAbnormal Day(s):\n"
     for row in rows:
         print(' '.join(row[0].split()) + ' - {0}% errors'.format(row[1]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
